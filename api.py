8eb1e786908a1aad6130ee7204f8ff49cf4ac15d
Lisa394 <123haring@gmail.com>
2020-10-14 09:13:46 +0200

    code netjes gemaakt

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify, render_template
+from flask import jsonify
 
 import mysql.connector
 
@@ -8,19 +8,19 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
-
+#connection local database
 connection = mysql.connector.connect(
-    host='127.0.0.1', port='3307', database='checkin', user='root', password='123flappie')
+    host='127.0.0.1', port='3307', database='checkin', user='root', password='****')
 myCursor = connection.cursor(dictionary=True)
 
-
+#get all the checkins
 @app.route('/api/checkins', methods=['GET'])
 def checkins_fetch():
     myCursor.execute("SELECT * FROM checkins")
     myResults = myCursor.fetchall()
     return jsonify(myResults), 200
 
-
+#only get the students id, the team he is in and his feeling
 @app.route('/api/checkins/gevoel', methods=['GET'])
 def gevoel_fetch():
     myCursor.execute("SELECT User_ID, Squad, Gevoel FROM checkins")
@@ -28,18 +28,10 @@ def gevoel_fetch():
     return jsonify(myResults), 200
 
 
-   #@app.route('/api/checkins', methods=['POST'])
-   #def checkins_post():
-
-
-
 app.run()
 
 #http://127.0.0.1:5000/api/checkins
 #http://127.0.0.1:5000/api/checkins/gevoel
-
-#from mysql.connector import Error
-#from MySQLdb import connect
 
 #Columns titels
 #ID, Start_time, Completion_time, User_ID, Datum, Squad, Gevoel, Waarom_gevoel, Gedaan, Geleerd, Vandaag_doen, Vraag
