34bc524ffe2b6c7ffc09ce105c5dae1f7195d0cf
Lisa394 <123haring@gmail.com>
2020-11-04 08:03:44 +0100

    Get request uitgebreid zodat er gezocht kan worden per user

--- api.py
+++ api.py
@@ -10,28 +10,36 @@ CORS(app)
 app.config["DEBUG"] = True
 
 connection = mysql.connector.connect(
-    host='127.0.0.1', port='3307', database='checkin', user='root', password='****')
+    host='127.0.0.1', port='3307', database='checkin', user='root', password='***')
 myCursor = connection.cursor(dictionary=True)
 
 
 @app.route('/api/checkins', methods=['GET'])
 def checkins_fetch():
-    myCursor.execute("SELECT * FROM checkins")
+    if 'user_id' in request.args:
+        user_id = request.args['user_id']
+        myCursor.execute("SELECT * FROM checkins WHERE User_ID = {}".format(user_id))
+    else:
+        myCursor.execute("SELECT * FROM checkins")
     myResults = myCursor.fetchall()
     return jsonify(myResults), 200, {'ContentType': 'application/json'}
-
 
 @app.route('/api/checkins', methods=['POST'])
 def checkins_post():
     data = request.json
-    myCursor.execute("INSERT INTO checkins (Start_time, Completion_time, Date, Squad, Feeling, Why_feeling, Did, Learned, Todo, Question) VALUES ('{}', '{}', '{}', '{}','{}', '{}','{}','{}','{}','{}')".format(data['start_time'], data['end_time'], data['date'], data['team'], data['feeling'], data['why_feeling'], data['did'], data['learned'], data['todo'], data['question']))
+    myCursor.execute("INSERT INTO checkins (Start_time, User_ID, Date, Squad, Feeling, Why_feeling, Did, Learned, Todo, Question) VALUES ('{}', '{}', '{}', '{}', '{}','{}', '{}','{}','{}', '{}')".format(data['start_time'], data['student_id'], data['date'], data['team'], data['feeling'], data['why_feeling'], data['did'], data['learned'], data['todo'], data['question']))
     connection.commit()
-    return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
+    return jsonify({'success': True}), 201, {'ContentType': 'application/json'}
 
+@app.route('/api/checkins/<int:Checkin_ID>', methods=['PUT'])
+def checkins_update(Checkin_ID):
+    myCursor.execute("")
+    connection.commit()
+    return jsonify({'success': True}), 202, {'ContentType': 'application/json'}
 
 app.run()
 
 # http://127.0.0.1:5000/api/checkins
-# http://127.0.0.1:5000/api/checkins/gevoel
+
 
 
--- fetch.js
+++ fetch.js
@@ -5,7 +5,7 @@ fetch('http://127.0.0.1:5000/api/checkins')
   var tbl = document.createElement("table");
   var tblHead = document.createElement("thead");
   var tblBody = document.createElement("tbody");
-  var keys = ['ID', 'Start_time', 'Completion_time', 'User_ID', 'Datum', 'Squad', 'Gevoel', 'Waarom_gevoel', 'Gedaan', 'Geleerd', 'Vandaag_doen', 'Vraag'];
+  var keys = ['ID', 'Start_time', 'Completion_time', 'User_ID', 'Date', 'Squad', 'Feeling', 'Why_Feeling', 'Did', 'Learned', 'Todo', 'Question'];
   var header = document.createElement("tr");
   keys.forEach(function (key) {
     //Object.keys(checkin).forEach(function (key) {
